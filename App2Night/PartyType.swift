c8cbd58edef6e496dfe8eb3fcbfdcbe6bd3a31ac
murph3d <22932116+murph3d@users.noreply.github.com>
2016-10-30 15:32:59 +0100

    Added Enums based on current backend

---
+++ App2Night/PartyType.swift
@@ -0,0 +1,22 @@
+//
+//  PartyType.swift
+//  App2Night
+//
+//  Created by Robin on 30.10.16.
+//  Copyright © 2016 DHBW. All rights reserved.
+//
+
+import Foundation
+
+public enum PartyType: String {
+    
+    // Bar
+    // Disco
+    // Forest
+    
+    case Bar = "Lokal"
+    case Disco = "Diskothek"
+    case Forest = "Abseits"
+    
+    
+}
