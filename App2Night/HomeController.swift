6f5a9be6306f77befac2a719a57be1f43458cca3
murph3d <22932116+murph3d@users.noreply.github.com>
2016-11-03 15:31:13 +0100

    Added LoginState (UI) programmatically, started refactoring SwaggerCommunication based on Token Authorization, added first CoreData files (+ Code)

---
+++ App2Night/HomeController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeController.swift
+//  App2Night
+//
+//  Created by Robin on 03.11.16.
+//  Copyright © 2016 DHBW. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UITabBarController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+	
+	
+}
+
