b4843a6389c01faa292ae9dd754cba9a86f962f7
murph3d <22932116+murph3d@users.noreply.github.com>
2016-11-06 02:24:58 +0100

    Updated project settings, clean-up, updated Core Data model

--- App2Night/ViewController.swift
+++ App2Night/ViewController.swift
@@ -1,17 +1,18 @@
 //
-//  HomeController.swift
+//  ViewController.swift
 //  App2Night
 //
-//  Created by Robin on 03.11.16.
+//  Created by Robin on 06.11.16.
 //  Copyright © 2016 DHBW. All rights reserved.
 //
 
 import UIKit
 
-class HomeController: UITabBarController {
+class ViewController: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		view.backgroundColor = .white
 	}
 	
 	
