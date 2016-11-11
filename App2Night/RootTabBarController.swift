bddbba6da6a6988cc5611dc1b48f8c48806beb37
murph3d <22932116+murph3d@users.noreply.github.com>
2016-11-11 23:39:04 +0100

    Finished Realm fetch/store of parties, added a few icons, continued work on ui

--- App2Night/RootTabBarController.swift
+++ App2Night/RootTabBarController.swift
@@ -1,14 +1,14 @@
 //
-//  PartyTableController.swift
+//  RootTabBarController.swift
 //  App2Night
 //
-//  Created by Robin on 07.11.16.
+//  Created by Robin on 11.11.16.
 //  Copyright © 2016 DHBW. All rights reserved.
 //
 
 import UIKit
 
-class PartyTableController: UITableViewController {
+class RootTabBarController: UITabBarController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
