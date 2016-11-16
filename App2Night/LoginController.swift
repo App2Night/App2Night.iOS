5a2e58a47e39dcc2157840e937afd1ca9d2b40b6
murph3d <22932116+murph3d@users.noreply.github.com>
2016-11-16 19:45:28 +0100

    Added some more icons, re-added login controller and cleaned up some code, updated frameworks

---
+++ App2Night/LoginController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginController.swift
+//  App2Night
+//
+//  Created by Robin on 16.11.16.
+//  Copyright © 2016 DHBW. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	
+	
+}
+
