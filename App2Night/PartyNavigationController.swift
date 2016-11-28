2168321780c7e09964f7235ae38e43b14a413376
murph3d <22932116+murph3d@users.noreply.github.com>
2016-11-28 15:41:24 +0100

    Implemented MapView, edited Cells, proper LocationManager, first implementation of PartyDetailView

---
+++ App2Night/PartyNavigationController.swift
@@ -0,0 +1,18 @@
+//
+//  PartyNavigationController.swift
+//  App2Night
+//
+//  Created by Robin on 28.11.16.
+//  Copyright © 2016 DHBW. All rights reserved.
+//
+
+import UIKit
+
+class PartyNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
